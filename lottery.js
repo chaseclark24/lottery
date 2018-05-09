589f9f2e9dc803276bc7095497fd5a63f62bf560
Chase <chase24@gmail.com>
2018-05-09 02:10:01 -0400

    Syntax Corrections

--- lottery.js
+++ lottery.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-var ticketInfo = function (text) {
+var ticketInfo = function(text) {
   if (text) {
     var o = JSON.parse(text);
     this.from = toString(o.from);
@@ -18,9 +18,9 @@ ticketInfo.prototype = {
   }
 };
 
-var lotteryContract = function() 
+var lottery = function() 
 {
-  LocalContractStorage.defineMapProerty(this, "lottery", 
+  LocalContractStorage.defineMapProerty(this, "lotteryDB", 
   {
     parse: function (text) 
     {
@@ -32,7 +32,7 @@ var lotteryContract = function()
     }
   });
 
-}
+};
 
 // users can participate in a lottery
 lottery.prototype = 
@@ -50,23 +50,23 @@ lottery.prototype =
     var from = Blockchain.transaction.from;
     var amount = Blockchain.transaction.value;
     var ticketBlock = new BigNumber(Blockchain.block.height);
-
-    if (amount != 1000000) {
+    //1 NAS = 10^18 wei
+    if (amount != 100000000000000000) {
       throw new Error("Tickets cost 0.1 Nebulas");
     }
     var ticket = new ticketInfo()
     ticket.from=from
     ticket.blockHeight=ticketBlock
   
-    this.lottery.put(ticketNumber, ticket)
+    this.lotteryDB.put(ticketNumber, ticket)
     ticketNumber += 1
 
-  }
+  },
 
   readTickets: function (blockHeight)
   {
-    return this.lottery.get(ticketNumber)
-  }
+    return this.lotteryDB.get(ticketNumber)
+  },
 
   payout: function (value, addr)
   {
@@ -83,6 +83,6 @@ lottery.prototype =
 
   }
 
+};
 
-
-module.exports = Lottery;
+module.exports = lottery;
